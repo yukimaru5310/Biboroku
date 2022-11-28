6594ad71f111fbbb82b1763e4e048a0333c007c8
yukimaru5310 <50508842+yukimaru5310@users.noreply.github.com>
2022-11-28 09:05:40 +0900

    [add] dashboard template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import DashboardContent from './dashboard/Dashboard'
 
-function App() {
-  return (
-    <div>
-      <p>Hello React.</p>
-    </div>
-  );
-}
+const App = () => {
+  return <DashboardContent />;
+};
 
 export default App;
+
+// function App() {
+//   return (
+//     <div>
+//       <p>Hello React.</p>
+//     </div>
+//   );
+// }
+
+// export default App;
