e6a166003846a49a85cc291889186ae142650791
yukimaru5310 <50508842+yukimaru5310@users.noreply.github.com>
2022-11-28 11:06:22 +0900

    Change Dashboard.js

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -72,10 +72,10 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
           easing: theme.transitions.easing.sharp,
           duration: theme.transitions.duration.leavingScreen,
         }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
+        width: theme.spacing(0),
+        // [theme.breakpoints.up('sm')]: {
+        //   width: theme.spacing(0),
+        // },
       }),
     },
   }),
@@ -118,7 +118,7 @@ function DashboardContent() {
               noWrap
               sx={{ flexGrow: 1 }}
             >
-              Dashboard
+              備忘録
             </Typography>
             <IconButton color="inherit">
               <Badge badgeContent={4} color="secondary">
@@ -161,41 +161,9 @@ function DashboardContent() {
         >
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} />
+ 
+
+            {/* <Copyright sx={{ pt: 4 }} /> */}
           </Container>
         </Box>
       </Box>
--- src/dashboard/listItems.js
+++ src/dashboard/listItems.js
@@ -13,33 +13,20 @@ import AssignmentIcon from '@mui/icons-material/Assignment';
 export const mainListItems = (
   <React.Fragment>
     <ListItemButton>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItemButton>
     <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
+
       <ListItemText primary="Orders" />
     </ListItemButton>
     <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
       <ListItemText primary="Customers" />
     </ListItemButton>
     <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
+
       <ListItemText primary="Reports" />
     </ListItemButton>
     <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
       <ListItemText primary="Integrations" />
     </ListItemButton>
   </React.Fragment>
@@ -51,21 +38,12 @@ export const secondaryListItems = (
       Saved reports
     </ListSubheader>
     <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
       <ListItemText primary="Current month" />
     </ListItemButton>
     <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
       <ListItemText primary="Last quarter" />
     </ListItemButton>
     <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
       <ListItemText primary="Year-end sale" />
     </ListItemButton>
   </React.Fragment>
